bcd1533740f2fecab56c0f156ca86194cfe7dd67
Miguelsantana2021 <marcelinosantosmiguel1@gmail.com>
2024-03-10 17:03:28 -0300

    meu decimo primeiro commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const botaoAbrir = document.querySelector("header.mobile > button")
+const botaoFechar = document.querySelector("header-mobile nav button ")
+
+const body = document.querySelector("body")
+const nav = document.querySelector(".header-mobile nav")
+
+console.log(body)
+
+botaoAbrir.addEventListener('click',abrirmenu)
+botaoAbrir.addEventListener('click',FecharMenu)
+
+function abrirmenu() {
+    body.classList.add("escurecer")
+    nav.classList.add("abrir")
+
+
+    
+}
+function FecharMenu(){
+    body.classList.remove("escurecer")
+    nav.classList.remove("abrir")
+
+}
